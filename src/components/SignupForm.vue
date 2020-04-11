70525bf25da745adb901788dc7ab5501665ddd2b
soyoung <soyoungp02@gmail.com>
2020-04-11 21:53:34 +0900

    회원가입 폼 마크업 및 데이터 바인딩, 이벤트 연결

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,9 +1,36 @@
 <template>
-	<div>signup form</div>
+	<form @submit.prevent="submitForm">
+		<div>
+			<label for="username">id: </label>
+			<input id="username" type="text" v-model="username" />
+		</div>
+		<div>
+			<label for="password">pw: </label>
+			<input id="password" type="text" v-model="password" />
+		</div>
+		<div>
+			<label for="nickname">nickname: </label>
+			<input id="nickname" type="text" v-model="nickname" />
+		</div>
+		<button type="submit">login</button>
+	</form>
 </template>
 
 <script>
-export default {};
+export default {
+	data() {
+		return {
+			username: '',
+			password: '',
+			nickname: '',
+		};
+	},
+	methods: {
+		submitForm() {
+			console.log('폼 제출');
+		},
+	},
+};
 </script>
 
 <style></style>
