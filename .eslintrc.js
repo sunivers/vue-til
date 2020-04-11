de14859b82b6f3a0304bbf4274581a91ba8dacf9
soyoung <soyoungp02@gmail.com>
2020-04-11 19:54:27 +0900

    린트 적용

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,30 +3,34 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   },
   rules: {
-    "no-console": "off",
-    // "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    // "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-    "prettier/prettier": ["error", {
-      singleQuote: true,
-      semi: true,
-      useTabs: true,
-      tabWidth: 2,
-      trailingComma: 'all',
-      printWidth: 80,
-      bracketSpacing: true,
-      arrowParens: 'avoid',
-    },]
+    'no-console': 'off',
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // .prettierrc 파일을 따로 만들지 않고 eslint 설정파일에 prettier 옵션을 넣는 이유는 eslint 규칙과 충돌할 경우 eslint 규칙이 우선시 되어야 하기 때문.
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        useTabs: true,
+        tabWidth: 2,
+        trailingComma: 'all',
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: 'avoid'
+      }
+    ]
   },
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
       ],
       env: {
         jest: true
