13aa7acf0f76980e9e3fb4cab9ab7d33910fb1b6
soyoung <soyoungp02@gmail.com>
2020-04-11 20:16:52 +0900

    페이지 컴포넌트 생성 및 연결

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,19 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import LoginPage from '@/views/LoginPage';
+import SignupPage from '@/views/SignupPage';
 
 Vue.use(VueRouter);
 
-export default new VueRouter();
+export default new VueRouter({
+	routes: [
+		{
+			path: '/login',
+			component: LoginPage,
+		},
+		{
+			path: '/signup',
+			component: SignupPage,
+		},
+	],
+});
