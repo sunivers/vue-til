b7d34bdd98d6ee040c065d1c4e316882a3e126eb
soyoung <soyoungp02@gmail.com>
2020-04-11 19:57:41 +0900

    파일 절대경로로 찾도록 설정 (vscode용 설정)

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+	<div id="app">
+		<img alt="Vue logo" src="./assets/logo.png" />
+		<HelloWorld msg="Welcome to Your Vue.js App" />
+	</div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue';
+import AppHeader from '@/components/common/AppHeader.vue';
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld
-  },
-  created() {
-    console.log(1)
-  }
+	name: 'App',
+	components: {
+		HelloWorld,
+		AppHeader,
+	},
+	created() {
+		const a = 1;
+		console.log('created');
+	},
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
