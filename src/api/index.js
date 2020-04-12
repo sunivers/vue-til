47d99fa794d52a27c9ce6c953b9bcddf5a711659
soyoung <soyoungp02@gmail.com>
2020-04-12 14:55:54 +0900

    env 파일 설정 및 Vue CLI의 env 파일 규칙

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-	baseURL: 'http://localhost:3000/',
+	// Vue CLI 2.x 버전에서는 .env 설정 값들을 들고오기 위해서는 웹팩에 따로 설정해줬어야 했는데
+	// Vue CLI 3 이후 버전에서는 'VUE_APP' 접두사를 붙인 변수는 자동으로 로드됨.
+	baseURL: process.env.VUE_APP_API_URL,
 });
 
 function registerUser(userData) {
