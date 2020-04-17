c156fbd90832af3136c611fa054fc804facc49ea
soyoung <soyoungp02@gmail.com>
2020-04-17 23:34:30 +0900

    스타일링 코드 추가

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,26 +1,31 @@
 <template>
-	<form @submit.prevent="submitForm">
-		<div>
-			<label for="username">id: </label>
-			<input
-				id="username"
-				type="text"
-				v-model="username"
-				:class="{ 'not-valid': username !== '' && !isUsernameValid }"
-			/>
-			<p class="error-text" v-if="username !== '' && !isUsernameValid">
-				올바른 메일주소가 아닙니다.
-			</p>
+	<div class="contents">
+		<div class="form-wrapper form-wrapper-sm">
+			<form @submit.prevent="submitForm" class="form">
+				<div>
+					<label for="username">id:</label>
+					<input id="username" type="text" v-model="username" />
+					<p class="validation-text">
+						<span class="warning" v-if="!isUsernameValid && username">
+							Please enter an email address
+						</span>
+					</p>
+				</div>
+				<div>
+					<label for="password">pw:</label>
+					<input id="password" type="text" v-model="password" />
+				</div>
+				<button
+					:disabled="!isUsernameValid || !password"
+					type="submit"
+					class="btn"
+				>
+					로그인
+				</button>
+			</form>
+			<p class="log">{{ logMessage }}</p>
 		</div>
-		<div>
-			<label for="password">pw: </label>
-			<input id="password" type="text" v-model="password" />
-		</div>
-		<button :disabled="!isUsernameValid || !password" type="submit">
-			로그인
-		</button>
-		<p>{{ logMessage }}</p>
-	</form>
+	</div>
 </template>
 
 <script>
--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,33 +1,24 @@
 <template>
-	<form @submit.prevent="submitForm">
-		<div>
-			<label for="username">id: </label>
-			<input
-				id="username"
-				type="text"
-				v-model="username"
-				:class="{ 'not-valid': username !== '' && !isUsernameValid }"
-			/>
-			<p class="error-text" v-if="username !== '' && !isUsernameValid">
-				올바른 메일주소가 아닙니다.
-			</p>
+	<div class="contents">
+		<div class="form-wrapper form-wrapper-sm">
+			<form @submit.prevent="submitForm" class="form">
+				<div>
+					<label for="username">id: </label>
+					<input id="username" type="text" v-model="username" />
+				</div>
+				<div>
+					<label for="password">pw: </label>
+					<input id="password" type="text" v-model="password" />
+				</div>
+				<div>
+					<label for="nickname">nickname: </label>
+					<input id="nickname" type="text" v-model="nickname" />
+				</div>
+				<button type="submit" class="btn">회원 가입</button>
+			</form>
+			<p class="log">{{ logMessage }}</p>
 		</div>
-		<div>
-			<label for="password">pw: </label>
-			<input id="password" type="text" v-model="password" />
-		</div>
-		<div>
-			<label for="nickname">nickname: </label>
-			<input id="nickname" type="text" v-model="nickname" />
-		</div>
-		<button
-			:disabled="!isUsernameValid || !password || !nickname"
-			type="submit"
-		>
-			회원 가입
-		</button>
-		<p>{{ logMessage }}</p>
-	</form>
+	</div>
 </template>
 
 <script>
