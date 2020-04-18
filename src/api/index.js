49fa33e978978fae6e936c866d6dc5c45c9db7cd
soyoung <soyoungp02@gmail.com>
2020-04-18 23:36:13 +0900

    axios interceptors를 이용한 HTTP 헤더 설정

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,16 @@
 import axios from 'axios';
-import store from '../store';
+import { setInterceptors } from './common/interceptors';
 
-const instance = axios.create({
-	// Vue CLI 2.x 버전에서는 .env 설정 값들을 들고오기 위해서는 웹팩에 따로 설정해줬어야 했는데
-	// Vue CLI 3 이후 버전에서는 'VUE_APP' 접두사를 붙인 변수는 자동으로 로드됨.
-	baseURL: process.env.VUE_APP_API_URL,
-	headers: {
-		Authorization: store.state.token,
-	},
-});
+function createInstance() {
+	const instance = axios.create({
+		// Vue CLI 2.x 버전에서는 .env 설정 값들을 들고오기 위해서는 웹팩에 따로 설정해줬어야 했는데
+		// Vue CLI 3 이후 버전에서는 'VUE_APP' 접두사를 붙인 변수는 자동으로 로드됨.
+		baseURL: process.env.VUE_APP_API_URL,
+	});
+	return setInterceptors(instance);
+}
+
+const instance = createInstance();
 
 function registerUser(userData) {
 	return instance.post('signup', userData);
