6245b8b3566a3fbb603edd35f8a60bfd505d857c
soyoung <soyoungp02@gmail.com>
2020-04-23 22:16:30 +0900

    학습 노트 수정 기능 구현

--- src/api/posts.js
+++ src/api/posts.js
@@ -5,6 +5,11 @@ function fetchPosts() {
 	return posts.get('/');
 }
 
+// 특정 학습 노트 조회 API
+function fetchPost(postId) {
+	return posts.get(postId);
+}
+
 // 학습 노트 데이터 생성 API
 function createPost(postData) {
 	return posts.post('/', postData);
@@ -15,4 +20,9 @@ function deletePost(postId) {
 	return posts.delete(postId);
 }
 
-export { fetchPosts, createPost, deletePost };
+// 학습 노트 수정 API
+function editPost(postId, postData) {
+	return posts.put(postId, postData);
+}
+
+export { fetchPosts, fetchPost, createPost, deletePost, editPost };
---
+++ src/views/PostEditPage.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		<PostEditForm></PostEditForm>
+	</div>
+</template>
+
+<script>
+import PostEditForm from '@/components/posts/PostEditForm.vue';
+export default {
+	components: {
+		PostEditForm,
+	},
+};
+</script>
