423ba81679388b3702b70053f30c220398fef519
soyoung <soyoungp02@gmail.com>
2020-04-24 22:19:24 +0900

    라우터 네비게이션 가드 적용

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import store from '../store';
 
 Vue.use(VueRouter);
 
-export default new VueRouter({
+const router = new VueRouter({
 	mode: 'history',
 	routes: [
 		{
@@ -21,18 +22,33 @@ export default new VueRouter({
 		{
 			path: '/main',
 			component: () => import('@/views/MainPage'),
+			meta: { auth: true },
 		},
 		{
 			path: '/add',
 			component: () => import('@/views/PostAddPage'),
+			meta: { auth: true },
 		},
 		{
 			path: '/post/:id',
 			component: () => import('@/views/PostEditPage'),
+			meta: { auth: true },
 		},
 		{
 			path: '*',
 			component: () => import('@/views/NotFoundPage'),
+			meta: { auth: true },
 		},
 	],
 });
+
+router.beforeEach((to, from, next) => {
+	if (to.meta.auth && !store.getters.isLogin) {
+		console.log('인증이 필요합니다.');
+		next('/login');
+		return;
+	}
+	next();
+});
+
+export default router;
