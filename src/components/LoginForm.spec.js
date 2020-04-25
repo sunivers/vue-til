d61437876fa1ac54252b7eef1d60aa71fcd899c8
soyoung <soyoungp02@gmail.com>
2020-04-26 02:18:39 +0900

    로그인 컴포넌트 테스트 코드 작성

--- src/components/LoginForm.spec.js
+++ src/components/LoginForm.spec.js
@@ -1,8 +1,31 @@
-import { sum } from './math';
+// import Vue from 'vue';
+import { shallowMount } from '@vue/test-utils';
+import LoginForm from './LoginForm.vue';
 
-describe('math.js', () => {
-	test('10 + 20 = 30', () => {
-		const result = sum(10, 20);
-		expect(result).toBe(30);
+describe('LoginForm.vue', () => {
+	test('ID는 이메일 형식이 아니면 경고 메세지가 출력된다.', () => {
+		const wrapper = shallowMount(LoginForm, {
+			data() {
+				return {
+					username: 'test',
+				};
+			},
+		});
+		// const idInput = wrapper.find('#username');
+		// console.log(idInput.element.value);
+		const warningText = wrapper.find('.warning');
+		expect(warningText.exists()).toBeTruthy();
+	});
+	test('ID와 PW가 입력되지 않으면 로그인 버튼이 비활성화 된다.', () => {
+		const wrapper = shallowMount(LoginForm, {
+			data() {
+				return {
+					username: '',
+					password: '',
+				};
+			},
+		});
+		const button = wrapper.find('button');
+		expect(button.element.disabled).toBeTruthy();
 	});
 });
